9e6cd1b33efad74a0d0096105341be307975bdd1
Amit Aharoni <amit.sites@gmail.com>
2024-12-09 21:09:16 +0100

    Unistyles 3

--- styles/unistyles.ts
+++ styles/unistyles.ts
@@ -1,28 +1,27 @@
-import { UnistylesRegistry } from 'react-native-unistyles'
-import { lightTheme } from "./theme-light"
-import { breakpoints } from './breakpoints'
+import { StyleSheet } from "react-native-unistyles";
+import { lightTheme } from "./theme-light";
+import { breakpoints } from "./breakpoints";
 
 // if you defined breakpoints
-type AppBreakpoints = typeof breakpoints
+type AppBreakpoints = typeof breakpoints;
 
 // if you defined themes
 type AppThemes = {
-  light: typeof lightTheme,
-}
+  light: typeof lightTheme;
+};
 
 // override library types
-declare module 'react-native-unistyles' {
+declare module "react-native-unistyles" {
   export interface UnistylesBreakpoints extends AppBreakpoints {}
   export interface UnistylesThemes extends AppThemes {}
 }
 
 export function setupUnistyleRegistry() {
-  UnistylesRegistry
-    .addBreakpoints(breakpoints)
-    .addThemes({
-      light: lightTheme,
-    })
-    .addConfig({
-      initialTheme: 'light',
-    })
+  StyleSheet.configure({
+    settings: {
+      initialTheme: "light",
+    },
+    breakpoints,
+    themes: { light: lightTheme },
+  });
 }
